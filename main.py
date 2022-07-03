0bf6e7268322f639d299ff3d94c0e4e57d5e65ae
Daniel <saruman566@yahoo.de>
2022-07-03 20:36:37 +0200

    sqlite to postgres

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ Bootstrap(app)
 
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///storys.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 database = SQLAlchemy(app)
 
